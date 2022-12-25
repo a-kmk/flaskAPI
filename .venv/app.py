c4103fbfc7b578f22fccff7912b08399e6ddb9a9
Anna Kmieciak <anna@indigoconsulting.ca>
2022-12-24 21:24:01 -0500

    fixes

--- .venv/app.py
+++ .venv/app.py
@@ -9,25 +9,21 @@ app = Flask(__name__)
 
 # the endpoint is the /store, the function
 # associated with that endpoint is get_stores
-@app.get("/store") #http:127.0.0.1:5000/store #
+
+@app.get("/store") #
 def get_stores():
     return {"stores" :list(stores.values())}
 
-@app.get("/store/<string:store_id>") #
+@app.get("/store/<string:store_id>") 
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
          abort(404, message="Store not found")
 
-@app.get("/item/<string:item_id>")
-def get_item_in_store(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found")
+
     
-@app.post("/store") #
+@app.post("/store") 
 def create_store():
     store_data=request.get_json()
     if "name" not in store_data:
@@ -35,6 +31,9 @@ def create_store():
             400,
             message="Bad request. Ensure 'name' is included in the JSON payload",
         )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=("Store already exists."))
     store_id = uuid.uuid4().hex
     #dictrionary of 2 keys, name and items, name is taken from json
     store={**store_data, "id": store_id}
@@ -66,7 +65,7 @@ def create_item():
         ):
             abort(400, message=("Item already exists"))
     
-    if item_data["stores_id"] not in stores:
+    if item_data["store_id"] not in stores:
         abort(404, message = "Store not found")
         
     item_id = uuid.uuid4().hex
@@ -79,9 +78,20 @@ def create_item():
 def get_all_items():
     return {"items" :list(items.values())}
 
+@app.get("/item/<string:item_id>")
+def get_item_in_store(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found")
 
-
-
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
 
 
 
